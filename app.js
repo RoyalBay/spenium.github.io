bab7e536ef25e798104645d25163fec7fea93919
spencer <spencerkirklandd@gmail.com>
2026-04-22 17:44:42 -0700

    v6.2

--- app.js
+++ app.js
@@ -5,22 +5,36 @@ function save(){
   localStorage.setItem("users", JSON.stringify(users));
 }
 
-/* FOLLOW (FIXED + RELIABLE) */
+/* VERIFIED BADGE (RESTORED) */
+function verified(u){
+  return (users[u]?.followers?.length || 0) >= 100 ? " ◆" : "";
+}
+
+/* FOLLOW (unchanged core) */
 function follow(user){
   if(!users[user]) return;
 
   users[user].followers ||= [];
 
-  let list = users[user].followers;
-
-  if(list.includes(currentUser)){
-    users[user].followers = list.filter(x => x !== currentUser);
+  if(users[user].followers.includes(currentUser)){
+    users[user].followers = users[user].followers.filter(x => x !== currentUser);
   } else {
-    list.push(currentUser);
+    users[user].followers.push(currentUser);
   }
 
   save();
   load();
+}
+
+/* CLICK PROFILE → LEFT PANEL (FIXED BEHAVIOR) */
+function openProfile(user){
+  let u = users[user];
+  if(!u) return;
+
+  leftProfile.style.display = "block";
+  leftPic.src = u.pic || "";
+  leftName.innerHTML = "<b>@"+user+"</b>" + verified(user);
+  leftFollowers.innerText = "Followers: " + u.followers.length;
 }
 
 /* POST */
